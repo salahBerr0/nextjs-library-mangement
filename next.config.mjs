137e964007df4113a7f3d039a3980a54871f6e23
Ines Goutel <goutelines20@gmail.com>
2025-11-06 00:06:33 +0100

    BOOK-1 Configuration de l'intégration Jira

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -6,12 +6,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
 
-export default nextConfig;
->>>>>>> 68db3b80f6083bd0618fbd261b3873ad5528b887
--- postcss.config.js
+++ postcss.config.js
@@ -5,11 +5,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 68db3b80f6083bd0618fbd261b3873ad5528b887
